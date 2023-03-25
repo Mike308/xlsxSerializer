45d8cd89f81d518948868af39c1f2f33fc6cc442
Mikołaj Werner <m.werner9205@gmail.com>
2023-03-25 14:12:52 +0100

    added annotations

---
+++ src/main/java/org/xlsx/serializer/annotation/OmitField.java
@@ -0,0 +1,10 @@
+package org.xlsx.serializer.annotation;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OmitField {
+
+}
---
+++ src/main/java/org/xlsx/serializer/annotation/XLSXProperty.java
@@ -0,0 +1,9 @@
+package org.xlsx.serializer.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface XLSXProperty {
+    String name();
+}
